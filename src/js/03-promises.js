4a9fd64eee2c3ceb0f829f63d5ea9b8120c8c0c5
Leichenko Anna <leichenkoanna@MacBook-Air-Leichenko.local>
2022-08-27 22:53:16 +0200

    finish task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function onsubmit(e) {
+  e.preventDefault();
+  const { amount, delay, step } = e.target;
+
+  for (let i = 0; i < amount.value; i++) {
+    let promiseDelay = Number(delay.value) + Number(step.value) * i;
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+};
+
+form.addEventListener('submit', onsubmit);

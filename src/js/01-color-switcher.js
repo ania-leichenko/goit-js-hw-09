6102b4c5647e84e4d3d0a5919a8c2ba560bf0c77
Leichenko Anna <leichenkoanna@MacBook-Air-Leichenko.local>
2022-08-27 16:30:58 +0200

    finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timer = null;
 
+function getRandomHexColor() {
+ return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timer = setInterval(function () {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
